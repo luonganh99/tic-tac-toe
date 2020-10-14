db49853ec4f46a5e4dcbf944bafa06dbf14c7e23
luonganh99 <nguyenhuuluonganh@gmail.com>
2020-10-14 20:27:25 +0700

    Updated location each move

--- src/components/MoveList.tsx
+++ src/components/MoveList.tsx
@@ -14,20 +14,22 @@ type Props = {
 const MoveList: React.FC<Props> = ({history, isAscending, onJumpToClick}) => {
     let moves : Array<JSX.Element> = [];
 
-    const renderMoves = (move: number) => {
+    const renderMoves = (step: History, move: number) => {
         const desc = move ? 'Go to move #' + move : 'Go to game start';
+        const X = step.currentPosition ? Math.floor(step.currentPosition / 3) : '';
+        const Y = step.currentPosition ? step.currentPosition % 3 : '';
         moves.push(<li key={move}>
-            <button className='btn btn-primary' onClick={() => onJumpToClick(move)}>{desc}</button>
+            <button className='btn btn-primary' onClick={() => onJumpToClick(move)}>{desc} {step.currentPosition && `(${X} - ${Y})`}</button>
         </li>)
     }
 
     if (isAscending) {
         for(let move = 0; move < history.length; move++) {
-            renderMoves(move);
+            renderMoves(history[move], move);
         }
     } else {
        for(let move = history.length - 1; move >=0; move--) {
-           renderMoves(move);
+           renderMoves(history[move], move);
        }
     }
 
